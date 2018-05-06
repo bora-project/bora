f81621418a09e80e7943ae1086e43a14e3fd40f0
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2018-05-05 23:04:19 -0300

    Fix - return missed.

--- routes/slack.js
+++ routes/slack.js
@@ -6,6 +6,7 @@ router.post('/challenge', function (req, res, next) {
   if (req.body.token != process.env.VERIFICATION_TOKEN) {
     res.status(401);
     res.send('Invalid token');
+    return;
   }
   
   res.status(200);
