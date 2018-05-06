b3f12369fab4e9e40a603148ec7fc070d43479f2
felipe-tomino <34447259+felipe-tomino@users.noreply.github.com>
2018-05-06 04:20:51 -0300

    Update messageParser.js

--- business/messageParser.js
+++ business/messageParser.js
@@ -31,8 +31,12 @@ function getTime() {
 
 messageParams.parse = function(inputMsg) {
 	var msg = inputMsg.split(" ");
+	var commandIndex = 0;
+	if (msg[0].toLowerCase() == "bora" || msg[0].toLowerCase() == "@bora")
+		commandIndex = 1;
+
 	var params = {
-					"action": msg[0],
+					"action": msg[commandIndex],
 					"date": getToday(),
 					"time": getTime(),
 					"txt": ""
@@ -41,7 +45,7 @@ messageParams.parse = function(inputMsg) {
 	var timeRegex = /^([0-9]|0[0-9]|1[0-9]|2[0-3])(?:(?:h|:)([0-5][0-9])?)?$/;
 	var dateRegex = /^([0-9]|0[0-9]|1[0-9]|2[0-9]|3[0-1])(?:\/|\-|\.)([0-9]|0[0-9]|1[0-2])(?:(?:\/|\-|\.)(\d\d(?:\d\d)?)?)?$/;
 
-	for (var i = msg.length - 1; i >= 1; i--) {
+	for (var i = msg.length - 1; i > commandIndex; i--) {
 		// se o parametro bater a regex de hora
 		if (timeRegex.test(msg[i])) {
 			var timeArray = timeRegex.exec(msg[i]);
@@ -75,7 +79,7 @@ messageParams.parse = function(inputMsg) {
 		// se nao der match em data nem horario
 		else {
 			if (msg[i].toLowerCase() != "bora" && msg[i].toLowerCase() != "@bora")
-			params["txt"] = msg[i] + " " + params["txt"];
+				params["txt"] = msg[i] + " " + params["txt"];
 		}
 	}
 
