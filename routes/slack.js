400a53f5fe5cdbddcf47c0b35766aebc5a78d5d1
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2018-05-06 01:05:58 -0300

    dotenv slack token var name

--- routes/slack.js
+++ routes/slack.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* Respond Slack with challenge paramenter. */
 router.post('/challenge', function (req, res, next) {
-  if (req.body.token != process.env.VERIFICATION_TOKEN) {
+  if (req.body.token != process.env.SLACK_VERIFICATION_TOKEN) {
     res.status(401);
     res.send('Invalid Token\n');
     return;
@@ -19,13 +19,13 @@ router.post('/challenge', function (req, res, next) {
 });
 
 const create_slack_event_adapter = require('@slack/events-api').createSlackEventAdapter;
-const slack_events = create_slack_event_adapter(process.env.VERIFICATION_TOKEN);
+const slack_events = create_slack_event_adapter(process.env.SLACK_VERIFICATION_TOKEN);
 
 router.post('/challenge', slack_events.expressMiddleware());
 
 slack_events.on('message', function(event){
   console.log(`Bora recebeu uma mensagem: de @${event.user} em #${event.channel}`);
   console.log(`>>> ${event.text}`);
-})
+});
 
 module.exports = router;
