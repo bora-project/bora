731cd1f0df725eb9bed7b72d6b6fa30f3e641573
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2018-05-06 01:22:36 -0300

    refactor of routes/slack.js

--- routes/slack.js
+++ routes/slack.js
@@ -3,30 +3,12 @@ var router = express.Router();
 
 const { WebClient } = require('@slack/client');
 const access_token = process.env.SLACK_OAUTH_ACCESS_TOKEN;
-
 const web = new WebClient(access_token);
-
-/* Respond Slack with challenge paramenter. */
-router.post('/challenge', function (req, res, next) {
-  if (req.body.token != process.env.SLACK_VERIFICATION_TOKEN) {
-    res.status(401);
-    res.send('Invalid Token\n');
-    return;
-  }
-
-  if (req.body.challenge !== undefined) {
-    res.status(200);
-    res.send(req.body.challenge);
-    return;
-  }
-
-  next();
-});
 
 const create_slack_event_adapter = require('@slack/events-api').createSlackEventAdapter;
 const slack_events = create_slack_event_adapter(process.env.SLACK_VERIFICATION_TOKEN);
 
-router.post('/challenge', slack_events.expressMiddleware());
+router.post('/event_api', slack_events.expressMiddleware());
 
 slack_events.on('message', function(event){
   console.log(`Bora recebeu uma mensagem: de @${event.user} em #${event.channel}`);
