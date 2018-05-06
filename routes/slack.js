62867eef3e719d680b2af42e6282efc3846d2928
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2018-05-06 00:21:24 -0300

    slack event url

--- routes/slack.js
+++ routes/slack.js
@@ -3,19 +3,18 @@ var router = express.Router();
 
 /* Respond Slack with challenge paramenter. */
 router.post('/challenge', function (req, res, next) {
-  if (req.body.token != process.env.VERIFICATION_TOKEN) {
-    res.status(401);
-    res.send('Invalid token');
+  if (req.body.token == process.env.VERIFICATION_TOKEN) {
+    res.status(200);
+    res.send(req.body.challenge);
     return;
   }
   
-  res.status(200);
-  res.send(req.body.challenge);
+  next();
 });
 
 const create_slack_event_adapter = require('@slack/events-api').createSlackEventAdapter;
 const slack_events = create_slack_event_adapter(process.env.VERIFICATION_TOKEN);
-router.use('/event', slack_events.expressMiddleware());
+router.use('/challenge', slack_events.expressMiddleware());
 slack_events.on('message', function(event){
   console.log('Bora recebeu uma mensagem: de @${event.user} em #${event.channel}');
   console.log('>>> ${event.text}');
