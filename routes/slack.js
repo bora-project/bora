632053c57cd6989e2794973b9c50c089836b382d
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2018-05-05 22:21:19 -0300

    Challenge is checking incoming token

--- routes/slack.js
+++ routes/slack.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 /* Respond Slack with challenge paramenter. */
 router.post('/challenge', function (req, res, next) {
+  if (req.body.token != process.env.VERIFICATION_TOKEN) {
+    res.status(401);
+    res.send('Invalid token');
+  }
+  
   res.status(200);
   res.send(req.body.challenge);
 });
