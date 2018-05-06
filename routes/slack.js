b7d8a6803c6c18d997bd5124265110d5539e7f5d
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2018-05-05 22:10:55 -0300

    refactor of slack challenge

--- routes/slack.js
+++ routes/slack.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+/* Respond Slack with challenge paramenter. */
 router.post('/challenge', function (req, res, next) {
   res.status(200);
   res.send(req.body.challenge);
