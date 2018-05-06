c908f82ae0b06a5739c534a9567ae9e5e3dda4bb
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2018-05-06 00:39:44 -0300

    Slack receiving messages

--- routes/slack.js
+++ routes/slack.js
@@ -3,21 +3,27 @@ var router = express.Router();
 
 /* Respond Slack with challenge paramenter. */
 router.post('/challenge', function (req, res, next) {
-  if (req.body.token == process.env.VERIFICATION_TOKEN) {
+  if (req.body.token != process.env.VERIFICATION_TOKEN) {
+    return;
+  }
+
+  if (req.body.challenge !== undefined) {
     res.status(200);
     res.send(req.body.challenge);
     return;
   }
-  
+
   next();
 });
 
 const create_slack_event_adapter = require('@slack/events-api').createSlackEventAdapter;
 const slack_events = create_slack_event_adapter(process.env.VERIFICATION_TOKEN);
-router.use('/challenge', slack_events.expressMiddleware());
+
+router.post('/challenge', slack_events.expressMiddleware());
+
 slack_events.on('message', function(event){
-  console.log('Bora recebeu uma mensagem: de @${event.user} em #${event.channel}');
-  console.log('>>> ${event.text}');
+  console.log(`Bora recebeu uma mensagem: de @${event.user} em #${event.channel}`);
+  console.log(`>>> ${event.text}`);
 })
 
 module.exports = router;
