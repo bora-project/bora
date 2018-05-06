168f65d4ec0de537fa1430c8023db8f768cbab8d
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2018-05-05 22:11:40 -0300

    slack router in app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var slackRouter = require('./slack/challenge');
+var slackRouter = require('./routes/slack');
 
 var app = express();
 
